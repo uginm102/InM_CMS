64f980f9b90a52e65c816a044fa21611592aea43
Eugene Munyaneza <munyanezae@gmail.com>
2026-08-19 12:57:53 +0200

    Added cloudinary for file uploads.

--- config/plugins.ts
+++ config/plugins.ts
@@ -37,6 +37,22 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
         allowedTypes: allowedMediaTypes,
         deniedTypes: deniedExecutableTypes,
       },
+
+      // Cloudinary configuration
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {
+          folder: 'inm-strapi-production',
+        },
+        delete: {},
+      },
+
     },
   },
 });
